1b05a169abfe912596b8aa62bea09e77eab4d826
Mau-Aravena <mau.aravena.acevedo@gmail.com>
2024-06-24 11:42:44 -0400

    first commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Mau-Aravena', // Usually your GitHub org/user name.
+  organizationName: 'mau-aravena', // Usually your GitHub org/user name.
   projectName: 'Gestion-Conocimiento', // Usually your repo name.
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
